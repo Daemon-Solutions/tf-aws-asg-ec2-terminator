6ae024742427f53097d359249e1cea3d8c8e8ad3
Stephen Stonehouse <stephen.stonehouse@claranet.uk>
2019-01-29 11:26:35 +0000

    Update test and add SG for SSH and egress all.

--- tests/asg.tf
+++ tests/asg.tf
@@ -3,36 +3,63 @@ module "terminator" {
   name      = "terminator-customer-prod"
   slack_url = "https://hooks.slack.com/services/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"
 
-  fallback_alarm         = true
-  fallback_sns_topic_arn = "${aws_sns_topic.pagerduty_fallback.id}"
-
   auto_scaling_groups = [
     {
-      name               = "${module.asg.asg_name}"
+      asg_name           = "${module.asg.asg_name}"
       threshold          = "90"
       period             = "60"
       evaluation_periods = "2"
     },
   ]
-}
 
-resource "aws_sns_topic" "pagerduty_fallback" {
-  name = "customer-prod-pagerduty"
+  fallback_alarms = [
+    {
+      asg_name           = "${module.asg.asg_name}"
+      threshold          = "90"
+      period             = "60"
+      evaluation_periods = "10"
+    },
+  ]
 }
 
 module "asg" {
-  source    = "git::ssh://git@gogs.bashton.net/Bashton-Terraform-Modules/tf-aws-asg.git"
-  name      = "customer-prod-asg"
-  envname   = "terminator"
-  service   = "terminator"
-  ami_id    = "ami-25e7705c"
-  subnets   = "${module.vpc.public_subnets}"
-  user_data = "${data.template_cloudinit_config.config.rendered}"
+  source          = "git::ssh://git@gogs.bashton.net/Bashton-Terraform-Modules/tf-aws-asg.git"
+  name            = "customer-prod-asg"
+  envname         = "terminator"
+  service         = "terminator"
+  ami_id          = "ami-25e7705c"
+  security_groups = ["${aws_security_group.terminator.id}"]
+  subnets         = "${module.vpc.public_subnets}"
+  user_data       = "${data.template_cloudinit_config.config.rendered}"
 
   instance_type               = "t3.nano"
   associate_public_ip_address = true
   min                         = "1"
   max                         = "5"
+}
+
+resource "aws_security_group" "terminator" {
+  name        = "terminator"
+  description = "Allow all outbound and inbound SSH traffic"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terminator"
+  }
 }
 
 data "template_file" "script" {
